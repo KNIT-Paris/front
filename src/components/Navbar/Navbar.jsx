b17c8770af81fce2a30d83564c32d8ecbb00aba0
thomas1996 <thomas.kantahck@gmail.com>
2024-01-16 05:12:13 +0100

    create navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import './navbar.scss'
 
 export default function Navbar() {
   return (
     <div className="navbar__wrapper">
-        <div className="navbar">
-            <div className="logo">
-                <p>KNIT</p>
-            </div>
-            <ul>
-                <li>
-                    <Link to="/discover">Discover</Link></li>
-                <li>
-                    <Link to="/shop">Shop</Link>
-                    </li>
-            </ul>
+      <div className="navbar">
+        <div className="logo">
+          <p>KNIT</p>
         </div>
+        <ul className="pagesTabs">
+          <li>
+            <Link to="/discover">Discover</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+        </ul>
+        <div className="profileFunctions">
+          <div className="profile">
+            <img src="/assets/icons/SVG/user.svg" alt="" />
+          </div>
+          <div className="favorite">
+            <img src="/assets/icons/SVG/fav.svg" alt="" />
+          </div>
+          <div className="cart">
+            <img src="/assets/icons/SVG/cart.svg" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
